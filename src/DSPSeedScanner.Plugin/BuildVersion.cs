3defa486bf6de99a7d5d3b21494a6977d76955e7
Shy Alexander Tamir <shytamir@gmail.com>
2026-08-14 10:33:43 +0200

    Advance release line to 1.3

--- src/DSPSeedScanner.Plugin/BuildVersion.cs
+++ src/DSPSeedScanner.Plugin/BuildVersion.cs
@@ -2,8 +2,8 @@
 {
     internal static class BuildVersion
     {
-        public const string BepInPluginVersion = "1.0.0";
-        public const string PluginVersion = "1.0.0";
-        public const string ReleaseLabel = "1.0.0.local";
+        public const string BepInPluginVersion = "1.3.0";
+        public const string PluginVersion = "1.3.0";
+        public const string ReleaseLabel = "1.3.0.local";
     }
 }
